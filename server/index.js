3a042f23772edbada8027c58ba8e79597fc6904e
Hack Reactor Student <student@hackreactor.com>
2018-02-17 12:20:16 -0500

    renders data from db and added delete button

--- server/index.js
+++ server/index.js
@@ -21,8 +21,27 @@ app.post('/country', function(req, res) {
 })
 
 app.post('/datapost', function(req, res) {
-    console.log('db post request body ', req.body)
+    // console.log('db post request body ', req.body)
     db.saveToDb(req.body);
+    res.sendStatus(201);
+})
+
+app.get('/datapost', function(req, res) {
+    // console.log('db get request body ', req.body)
+    db.getAllData(function(err, data) {
+        if(err) {
+            console.log('error in getting all data from db ', err)
+            res.status(404)
+        }
+    //    console.log('results from get to db ', data)
+       res.status(201)
+       res.send(data)
+    })
+})
+
+app.post('/deleteItem', function(req, res) {
+    console.log('db delete request ', req.body)
+    db.deleteItem(req.body.name);
     res.sendStatus(201);
 })
 
